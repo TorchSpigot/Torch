6d68d87cfd3eb3671dba12c066b89b6c6aa60c24
BuildTools <unconfigured@null.spigotmc.org>
2016-06-25 06:11:55 +0800

    add org.spigotmc codes, not checked

---
+++ src/org/spigotmc/event/entity/EntityDismountEvent.java
@@ -0,0 +1,52 @@
+package org.spigotmc.event.entity;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.entity.EntityEvent;
+
+/**
+ * Called when an entity stops riding another entity.
+ *
+ */
+public class EntityDismountEvent extends EntityEvent implements Cancellable // Paper - implement Cancellable
+{
+
+    private static final HandlerList handlers = new HandlerList();
+    private boolean cancelled;
+    private final Entity dismounted;
+
+    public EntityDismountEvent(Entity what, Entity dismounted)
+    {
+        super( what );
+        this.dismounted = dismounted;
+    }
+
+    public Entity getDismounted()
+    {
+        return dismounted;
+    }
+
+    @Override
+    public HandlerList getHandlers()
+    {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList()
+    {
+        return handlers;
+    }
+
+    // Paper start - Implement cancellable methods
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+    // Paper end
+}
