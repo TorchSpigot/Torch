086c6638291977e02dc61a4dbd279683c6e09325
SotrForgotten <i@omc.hk>
2017-05-19 20:08:33 +0800

    Cleanup

--- sources/src/main/java/net/minecraft/server/WhiteList.java
+++ sources/src/main/java/net/minecraft/server/WhiteList.java
@@ -34,7 +34,7 @@ public class WhiteList extends JsonList<GameProfile, WhiteListEntry> {
     }
 
     protected String b(GameProfile profile) {
-        return authUUID() ? Caches.objectString(profile) : Caches.toLowerCase(profile.getName()); // Torch - use cache
+        return authUUID() ? profile.getId().toString() : Caches.toLowerCase(profile.getName()); // Torch - use cache
     }
 
     public GameProfile a(String s) {
